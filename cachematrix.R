0675301a41059fc4acb812a7a325de4010766bcb
Marcos-Antonio-Ribeiro <m30@uol.com.br>
2015-07-24 09:15:29 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+    ## If the inverse of x (matrix) has already been computed then
+    ## cacheSolve function returns it from the cache, otherwise it calls 
+    ## makeCacheMatrix function to do it
 
 ## Write a short comment describing this function
-
+    ## Strores x and its inverse in the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<<-x
+  inverse.m<<-solve(x)
 }
 
-
 ## Write a short comment describing this function
-
+    ## Check if the inverse of x (matrix) is already cached, if so, then returns it,
+    ## otherwise it calls makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!exists("m")) m<-c(0,0)
+  if(m.equals(x,m)) {
+    inverse.m
+  } else {
+    makeCacheMatrix(x)
+  }
+}
+
+    ## Returns TRUE if matrix a and b are identicals
+m.equals <- function(a, b) {
+  is.matrix(a)&& is.matrix(b) && dim(a) == dim(b)&& all(a == b)
 }
